6c5480533cbe424132fd294126505c6c9bf39fba
GitH-IDHRmsk <141799407+GitH-IDHRmsk@users.noreply.github.com>
2023-10-21 01:52:56 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+const now = new Date();
+const date = now.getDate();
+const  month = now.getMonth()+1;
+const year = now.getFullYear();
+
+const present = year+'年'+month+'年'+date+'日'
+
+console.log(present);
+
+
+
